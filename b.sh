3e5a4a3964ac14b6e0671c605258ee0a09fafcdb
walterclozet <walterclozet@email.com>
2023-07-23 17:01:24 +0800

    fix path error

--- b.sh
+++ b.sh
@@ -6,9 +6,10 @@ rm -r dist/data/
 rm dist/niconi
 cp -rp assets dist/
 cp -rp static dist/
+cp *.pem dist/
 mkdir -p dist/data/allstars.db/
-rm dist/static/*en*
-rm dist/static/*ko*
+rm dist/static/2d61e7b4e89961c7/*en*
+rm dist/static/2d61e7b4e89961c7/*ko*
 env CGO_ENABLED=0 GOOS=android GOARCH=arm64 go build -o dist/niconi main.go
 cd dist
 zip -r ../niconi.zip *
